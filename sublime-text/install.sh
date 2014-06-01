6a0be243d7e0b1321a094dfd99c89e30b73b9150
Kris McGinnes <kmcginnes@me.com>
2014-06-01 17:24:14 -0500

    Fix path for sublime

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -5,12 +5,12 @@ ZSH=$HOME/.dotfiles
 SUBLIME_FOLDER="$HOME/Library/Application Support/Sublime Text 3/"
 
 # Check that settings do not already exist
-if [ ! -e "$SUBLIME_FOLDER/Installed Packages" ]; then
-    mkdir -p "$SUBLIME_FOLDER/Installed Packages"
-    ln -s ~/.dotfiles/sublime-text/Installed\ Packages/ ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
+if [ ! -e "$SUBLIME_FOLDER/Installed Packages/" ]; then
+    mkdir -p "$SUBLIME_FOLDER/Installed Packages/"
+    ln -s "$ZSH/sublime-text/Installed Packages/" "$SUBLIME_FOLDER/Installed Packages/"
 fi
 
-if [ ! -e "$SUBLIME_FOLDER/Packages/User" ]; then
-    mkdir -p "$SUBLIME_FOLDER/Packages/User"
-    ln -s ~/.dotfiles/sublime-text/User/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+if [ ! -e "$SUBLIME_FOLDER/Packages/" ]; then
+    mkdir -p "$SUBLIME_FOLDER/Packages/"
+    ln -s "$ZSH/sublime-text/Packages/" "$SUBLIME_FOLDER/Packages/"
 fi
