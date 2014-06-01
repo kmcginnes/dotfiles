985af1034f58654e5adccdecc43808653b605654
Kris McGinnes <kmcginnes@me.com>
2014-06-01 18:16:27 -0500

    Fix sublime symlink

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -5,12 +5,9 @@ ZSH=$HOME/.dotfiles
 SUBLIME_FOLDER="$HOME/Library/Application Support/Sublime Text 3/"
 
 # Check that settings do not already exist
-if [ ! -e "$SUBLIME_FOLDER/Installed Packages/" ]; then
-    mkdir -p "$SUBLIME_FOLDER/Installed Packages/"
-    ln -s "$ZSH/sublime-text/Installed Packages/" "$SUBLIME_FOLDER/Installed Packages/"
-fi
+if [ ! -e "$SUBLIME_FOLDER" ]; then
+    mkdir -p "$SUBLIME_FOLDER"
 
-if [ ! -e "$SUBLIME_FOLDER/Packages/" ]; then
-    mkdir -p "$SUBLIME_FOLDER/Packages/"
-    ln -s "$ZSH/sublime-text/Packages/" "$SUBLIME_FOLDER/Packages/"
+	ln -s "$ZSH/sublime-text/Installed Packages/" "$SUBLIME_FOLDER/Installed Packages/"
+	ln -s "$ZSH/sublime-text/Packages/" "$SUBLIME_FOLDER/Packages/"
 fi
