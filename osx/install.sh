d59c1ae090d85bd669a9b1249ce97103c09928de
Kris McGinnes <kmcginnes@me.com>
2014-06-01 13:32:21 -0500

    Added a bunch of OSX apps

---
+++ osx/install.sh
@@ -0,0 +1,26 @@
+brew cask install google-chrome
+brew cask update google-chrome
+
+brew cask install github
+brew cask update github
+
+brew cask install hipchat
+brew cask update hipchat
+
+brew cask install kaleidoscope
+brew cask update kaleidoscope
+
+brew cask install onepassword
+brew cask update onepassword
+
+brew cask install evernote
+brew cask update evernote
+
+brew cask install the-unarchiver
+brew cask update the-unarchiver
+
+brew cask install vlc
+brew cask update vlc
+
+brew cask install vmware-fusion
+brew cask update vmware-fusion
