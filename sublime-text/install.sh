3dc33bdc2c3c77ff8fdfeb4f4109603d94ea2602
Kris McGinnes <kmcginnes@me.com>
2014-06-01 18:30:55 -0500

    Fix path for sublime

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -2,7 +2,7 @@ brew cask install sublime-text3
 brew cask update sublime-text3
 
 ZSH=$HOME/.dotfiles
-SUBLIME_FOLDER="$HOME/Library/Application Support/Sublime Text 3/"
+SUBLIME_FOLDER="$HOME/Library/Application Support/Sublime Text 3"
 
 # Check that settings do not already exist
 if [ ! -e "$SUBLIME_FOLDER" ]; then
