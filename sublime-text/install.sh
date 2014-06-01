d53e75299cc65550a9bdc31bf92dd242c9d8a833
Kris McGinnes <kmcginnes@me.com>
2014-06-01 16:13:51 -0500

    Added symlinks for sublime

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -1,2 +1,6 @@
 brew cask install sublime-text3
 brew cask update sublime-text3
+
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/
+ln -s ~/.dotfiles/sublime-text/Installed\ Packages/ ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
+ln -s ~/.dotfiles/sublime-text/User/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
