8e183f492e64f600531bee3cfefd0fc81784b78d
Kris McGinnes <kmcginnes@me.com>
2014-06-01 18:48:57 -0500

    Fixed symlink path

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -8,6 +8,6 @@ SUBLIME_FOLDER="$HOME/Library/Application Support/Sublime Text 3"
 if [ ! -e "$SUBLIME_FOLDER" ]; then
     mkdir -p "$SUBLIME_FOLDER"
 
-	ln -s "$ZSH/sublime-text/Installed Packages/" "$SUBLIME_FOLDER/Installed Packages/"
-	ln -s "$ZSH/sublime-text/Packages/" "$SUBLIME_FOLDER/Packages/"
+	ln -s "$ZSH/sublime-text/Installed Packages/" "$SUBLIME_FOLDER"
+	ln -s "$ZSH/sublime-text/Packages/" "$SUBLIME_FOLDER"
 fi
