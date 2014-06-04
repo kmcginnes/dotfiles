2778e24abc44174b316996b202084fab58660945
Kris McGinnes <kmcginnes@me.com>
2014-06-03 20:08:56 -0500

    Trying to get sublime working

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
+
 brew cask install sublime-text3
 brew cask update sublime-text3
 
-ZSH=$HOME/.dotfiles
+# symlink settings in
 SUBLIME_FOLDER="$HOME/Library/Application Support/Sublime Text 3"
 
 # Check that settings do not already exist
 if [ ! -e "$SUBLIME_FOLDER" ]; then
-    mkdir -p "$SUBLIME_FOLDER"
+    mkdir -p "$SUBLIME_FOLDER/Packages"
 
-	ln -s "$ZSH/sublime-text/Installed Packages/" "$SUBLIME_FOLDER"
-	ln -s "$ZSH/sublime-text/Packages/" "$SUBLIME_FOLDER"
+	ln -s "$ZSH/sublime-text/User" "$SUBLIME_FOLDER/Packages"
 fi
