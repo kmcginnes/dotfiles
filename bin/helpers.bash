95188e57a0a0c250cce0f2c1b8bf3777e71909e1
Kris McGinnes <kmcginnes@me.com>
2014-07-20 10:49:42 -0500

    Added some new commands

--- bin/helpers.bash
+++ bin/helpers.bash
@@ -11,6 +11,8 @@ git diff --name-only | grep .config | xargs dos2unix
 git svn clone -s --prefix=origin/ http://phoenix.watchguardvideo.local/reposWin/Vermillion Vermillion
 git svn clone -s --prefix=origin/ http://phoenix.watchguardvideo.local/reposWin/Packages/WatchGuard/FoxBurner.Abstractions FoxBurner.Abstractions
 
+git branch elx2 remotes/origin/ELX2_0
+
 svn rm http://phoenix/reposWin/Vermillion/tags/sprint_3_review
 
 git svn tag sprint_7_review -m "Sprint 7 Review"
@@ -18,3 +20,5 @@ git svn tag sprint_7_review -m "Sprint 7 Review"
 set PATH=%PATH%;Z:\Dropbox\bin
 
 find ./ -name "*.sh" -exec chmod +x {} \;
+
+find ./ -name "*.targets" -exec git add -f {} \;
